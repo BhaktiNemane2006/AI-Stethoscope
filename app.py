c5c42051b77a16d0d103032f07b0c81beb01f4e8
Bhakti Nemane <bhaktinemane@gmail.com>
2026-04-14 13:26:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -260,41 +260,8 @@ if os.path.exists("history.csv"):
 # -----------------------------
 # WAVEFORM
 # -----------------------------
-st.subheader("📈 Live Waveform")
-
-placeholder = st.empty()
-for _ in range(100):
-    
-    if len(st.session_state.audio_data) > 500:
-        raw = st.session_state.audio_data
-    else:
-        raw = np.zeros(300)
-
-    filtered = heart_filter(raw)
-    filtered = filtered / (np.max(np.abs(filtered)) + 1e-6)
-
-    peaks, _ = find_peaks(filtered, distance=80, prominence=0.3)
-
-    if len(peaks) > 1:
-        bpm = int(60 * 1000 / np.mean(np.diff(peaks)))
-    else:
-        bpm = 0
-
-    with placeholder.container():
-        st.line_chart(filtered[-300:])
-
-    time.sleep(0.2)
-# -----------------------------
-# PLAY RECORDED AUDIO
-# -----------------------------
-st.subheader("🔊 Playback")
-
-if st.button("Play Recorded Sound"):
-    if len(st.session_state.audio_data) > 500:
-        audio_file = save_temp_audio(st.session_state.audio_data)
-        st.audio(audio_file)
-    else:
-        st.warning("No audio recorded yet")
+st.subheader("📈 Waveform")
+st.line_chart(filtered[-300:])
 # -----------------------------
 # ADVANCED GRAPHS
 # -----------------------------
